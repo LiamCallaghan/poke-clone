ae7c83e832500e1d6395cf7794fd93b006d67f78
Liam <ljcallaghan64@gmail.com>
2021-02-22 11:11:45 +0000

    test data displaying

--- src/component/pokedex/Pokedex.js
+++ src/component/pokedex/Pokedex.js
@@ -2,13 +2,28 @@ import React from 'react'
 
 class Pokedex extends React.Component {
   state = {
-
+    number: 1,
+    name: 'test',
+    sprite: 'https://cdn.traction.one/pokedex/pokemon/5.png',
+    description: 'default',
+    types: ['fire', 'water'],
   }
 
   render() {
     if (!this.state) return null
+    const { number, name, sprite, description, types } = this.state
     return (
-      <div><p>Hello world.</p></div>
+      <div>
+        <p>{number}</p>
+        <p>{name}</p>
+        <img src={sprite} />
+        <p>{description}</p>
+        <ul>
+          {types.map(type => {
+            return <li key={type}>{type}</li>
+          })}
+        </ul>
+      </div>
     )
   }
 }
