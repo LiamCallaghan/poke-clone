184e25520ee80ebd89f1d613ec71ec83320583da
Liam <ljcallaghan64@gmail.com>
2021-02-25 16:14:14 +0000

    random flavour text and sprite flip

--- src/component/pokedex/Pokedex.js
+++ src/component/pokedex/Pokedex.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Select from 'react-select'
-import { getPokemonByNumber } from '../../lib/api'
+import { getPokemonByNumber, getPokemonInfo, getEvolutionChain } from '../../lib/api'
 
 class Pokedex extends React.Component {
   state = {
     number: 1,
     name: 'test',
-    sprite: 'https://cdn.traction.one/pokedex/pokemon/5.png',
+    sprite: '',
+    otherSprite: '',
     description: 'default',
+    flavorText: 'default',
     evolutionLine: [ 'Squirtle', 'Froakie', 'Charmeleon'],
     types: ['fire', 'water'],
     typeArray: [
@@ -29,35 +31,37 @@ class Pokedex extends React.Component {
       { value: 'psychic', label: 'Psychic' }, 
       { value: 'rock', label: 'Rock' }, 
       { value: 'steel', label: 'Steel' } ],
+    count: 0,
     value: '',
-    value2: 0,
+    value2: '',
     trueValue: '',
   }
 
   async componentDidMount() {
-    const response = await getPokemonByNumber(this.state.number)
-    console.log(response.data)
-    this.setState({
-      name: response.data.name,
-      sprite: response.data.sprites.front_default,
-    })
-    if (response.data.types.length > 1) {
-      this.setState({
-        types: [response.data.types[0].type.name, response.data.types[1].type.name],
-      })
-    } else {
-      this.setState({
-        types: [response.data.types[0].type.name],
-      })
-    }
-    console.log(this.state.types)
+    this.newPokemon(this.state.number)
   }
 
   async newPokemon(newID){
     const response = await getPokemonByNumber(newID)
+    const info = await getPokemonInfo(newID)
+    const evoChain = await getEvolutionChain(info.data.evolution_chain.url)
+    console.log(evoChain.data.chain)
+    console.log(response.data)
+    // this.evolutionTest(evoChain.data.chain.evolves_to)
+    const randomTextArray = []
+    info.data.flavor_text_entries.forEach( entry => {
+      if (entry.language.name === 'en') {
+        randomTextArray.push(entry.flavor_text)
+      }
+    })
+    // console.log(randomTextArray)
     this.setState({
       name: response.data.name,
       sprite: response.data.sprites.front_default,
+      otherSprite: response.data.sprites.back_default,
+      number: response.data.id,
+      description: info.data.genera[7].genus,
+      flavorText: randomTextArray[Math.floor(Math.random() * randomTextArray.length)],
     })
     if (response.data.types.length > 1) {
       this.setState({
@@ -68,8 +72,24 @@ class Pokedex extends React.Component {
         types: [response.data.types[0].type.name],
       })
     }
-    console.log(this.state.types)
   }
+
+  // evolutionTest(string){
+  //   const arr = []
+  //   function evolutionNest(nestString){
+  //     if (nestString.length > 0) {
+  //       console.log(nestString[0].species.name)
+  //       arr.push(string[0].species.name)
+  //       evolutionNest(nestString[0].evolves_to)
+  //     } else {
+  //       console.log(arr)
+  //       this.setState({
+  //         evolutionLine: arr,
+  //       })
+  //     }
+  //   }
+  //   evolutionNest(string)
+  // }
 
   handleChange = (event) => {
     const letter = event.target.value
@@ -86,7 +106,7 @@ class Pokedex extends React.Component {
     })
   }
   handleSubmit = (event) => {
-    console.log(this.state.trueValue)
+    this.newPokemon(this.state.trueValue)
     this.setState({
       value: '',
       value2: 0,
@@ -98,41 +118,77 @@ class Pokedex extends React.Component {
     this.setState({
       number: (this.state.number - 1),
     })
-    this.newPokemon(this.state.number - 1)
+    if (this.state.number === 1) {
+      this.setState({
+        number: 898,
+      })
+      this.newPokemon(898)
+    } else {
+      this.newPokemon(this.state.number - 1)
+    }
   }
 
   handleClick2 = () => {
     this.setState({
       number: (this.state.number + 1),
     })
-    this.newPokemon(this.state.number + 1)
+    if (this.state.number === 898) {
+      this.setState({
+        number: 1,
+      })
+      this.newPokemon(1)
+    } else {
+      this.newPokemon(this.state.number + 1)
+    }
+  }
+
+  handleClick3 = () => {
+    if (this.state.count === 0) {
+      const flip = this.state.otherSprite
+      const flop = this.state.sprite
+      this.setState({
+        count: 1,
+        sprite: flip,
+        otherSprite: flop,
+      })
+    } else {
+      const flip = this.state.otherSprite
+      const flop = this.state.sprite
+      this.setState({
+        count: 0,
+        sprite: flip,
+        otherSprite: flop,
+      })
+    }
   }
 
   render() {
     if (!this.state) return null
-    const { number, name, sprite, description, types, evolutionLine, typeArray, value, value2 } = this.state
+    const { number, name, sprite, description, flavorText, types, evolutionLine, typeArray, value, value2 } = this.state
     return (
       <>
         <section>
           <div className='side1'>
             <img className='pokemonSprite' src={sprite} />
             <div className='evoBox'>
-              <a>{evolutionLine[0]}</a>
-              <a>{evolutionLine[1]}</a>
-              <a>{evolutionLine[2]}</a>
+              {evolutionLine.map(evolution => {
+                return <a key={evolution}>{evolution}</a>
+              })}
             </div>
             <div>
               <button onClick={this.handleClick}>Previous</button>
               <button onClick={this.handleClick2}>Next</button>
+              <button onClick={this.handleClick3}>Flip</button>
             </div>
           </div>
           <div className='side2'>
             <p>{number}</p>
             <p>{name}</p>
-            <p>{description}</p>
+            <p>The {description}.</p>
+            <p>{flavorText}</p>
             <ul>
               {types.map(indType => {
-                return <li key={indType.slot}>{(indType)}</li>
+                return <li key={indType}>{(indType)}</li>
               })}
             </ul>
             <form onSubmit={this.handleSubmit}>
--- src/lib/api.js
+++ src/lib/api.js
@@ -6,3 +6,11 @@ const baseURL = 'https://pokeapi.co/api/v2'
 export const getPokemonByNumber = id => {
   return axios.get(`${baseURL}/pokemon/${id}`)
 }
+
+export const getPokemonInfo = id => {
+  return axios.get(`${baseURL}/pokemon-species/${id}`)
+}
+
+export const getEvolutionChain = URL => {
+  return axios.get(URL)
+}
