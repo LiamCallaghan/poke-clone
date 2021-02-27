08c80bc9f8b764991a2f43b899987554508c892d
Liam <ljcallaghan64@gmail.com>
2021-02-27 01:12:09 +0000

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # poke-clone
+
+A Pokedex clone built with React.
+
+![Finished Pokedex](https://i.imgur.com/m77TegH.png)
+
+## Deployed Link
+
+Coming soon!
+
+## Technologies used
+
+- React.JS
+- SCSS
+- JSX
+- Git
+- PokeAPI
+
+## Development Pictures and Code
+
+Final state organisation
+![State](https://i.imgur.com/8TfGrZe.png)
+
+Closed/Off Pokedex
+![Closed Pokedex](https://i.imgur.com/ooQEotU.png)
+
+Requesting the API
+![Component did mount function](https://i.imgur.com/35ZCkRH.png)
+
+The most complicated evolution tree
+![Eeveelutions](https://i.imgur.com/zFmxXEB.png)
+
+Function to show evolution trees
+![Convoluted Evolution trees](https://i.imgur.com/F96GwnX.png)
+
+Search by name and number inputs
+![Handle change](https://i.imgur.com/3uDpfhG.png)
+
+Front and Back View of sprites
+![Front](https://i.imgur.com/mitUTlY.png)
+![Back](https://i.imgur.com/qjquHyi.png)
+
+Rendered JSX
+![JSX](https://i.imgur.com/Pw2odTF.png)
+
+## Extra Features
+
+I plan on including the ability to search by first letter of pokemon name and type.
+
+## Thanks for reading!
--- src/component/pokedex/Pokedex.js
+++ src/component/pokedex/Pokedex.js
@@ -1,44 +1,27 @@
 import React from 'react'
-// import Select from 'react-select'
 import { getPokemonByNumber, getPokemonInfo, getEvolutionChain } from '../../lib/api'
 
 class Pokedex extends React.Component {
   state = {
     number: 1,
-    name: 'test',
-    sprite: '',
+    name: 'Bulbasaur',
+    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
     otherSprite: '',
-    description: 'default',
-    flavorText: 'default',
+    description: 'seed Pokemon',
+    flavorText: 'Loading...',
     evolutionIndex: [ 0, 1, 2],
-    evolutionLine: [ 'squirtle', 'froakie', 'charmeleon'],
-    evolutionSprites: [ 'test', 'default', 'testing'],
-    placeholderSprites: ['https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'],
-    types: ['fire', 'water'],
-    // typeArray: [
-    //   { value: 'water', label: 'Water' }, 
-    //   { value: 'bug', label: 'Bug' }, 
-    //   { value: 'dark', label: 'Dark' }, 
-    //   { value: 'dragon', label: 'Dragon' }, 
-    //   { value: 'electric', label: 'Electric' }, 
-    //   { value: 'fairy', label: 'Fairy' }, 
-    //   { value: 'fighting', label: 'Fighting' }, 
-    //   { value: 'fire', label: 'Fire' }, 
-    //   { value: 'flying', label: 'Flying' }, 
-    //   { value: 'ghost', label: 'Ghost' }, 
-    //   { value: 'grass', label: 'Grass' }, 
-    //   { value: 'ground', label: 'Ground' }, 
-    //   { value: 'ice', label: 'Ice' }, 
-    //   { value: 'normal', label: 'Normal' }, 
-    //   { value: 'poison', label: 'Poison' }, 
-    //   { value: 'psychic', label: 'Psychic' }, 
-    //   { value: 'rock', label: 'Rock' }, 
-    //   { value: 'steel', label: 'Steel' } ],
+    evolutionLine: [ 'bulbasaur', 'ivysaur', 'venusaur'],
+    evolutionSprites: [ 
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
+    ],
+    types: ['grass', 'poison'],
     count: 0,
     value: '',
     value2: '',
     trueValue: '',
-    loadCheck: '',
+    openCheck: 'closed',
   }
 
   async componentDidMount() {
@@ -49,10 +32,11 @@ class Pokedex extends React.Component {
     const response = await getPokemonByNumber(id)
     const info = await getPokemonInfo(id)
     const evoChain = await getEvolutionChain(info.data.evolution_chain.url)
-    // console.log(evoChain.data.chain)
+    // console.log(evoChain.data.chain.species.url.slice(-5).replace(/[^0-9]/g, ''))
     // console.log(response.data)
     // this.evolutionTest(evoChain.data.chain.evolves_to)
     const randomTextArray = []
+    const capitalName = (response.data.name).charAt(0).toUpperCase() + (response.data.name).slice(1)
     info.data.flavor_text_entries.forEach( entry => {
       if (entry.language.name === 'en') {
         randomTextArray.push(entry.flavor_text)
@@ -60,7 +44,7 @@ class Pokedex extends React.Component {
     })
     // console.log(randomTextArray)
     this.setState({
-      name: response.data.name,
+      name: capitalName,
       sprite: response.data.sprites.front_default,
       otherSprite: response.data.sprites.back_default,
       number: response.data.id,
@@ -68,7 +52,7 @@ class Pokedex extends React.Component {
       flavorText: randomTextArray[Math.floor(Math.random() * randomTextArray.length)],
       evolutionIndex: [0],
       evolutionLine: [evoChain.data.chain.species.name],
-      evolutionSprites: [this.pokemonSprite(evoChain.data.chain.species.name)],
+      evolutionSprites: [this.pokemonSprite(evoChain.data.chain.species.url.slice(-5).replace(/[^0-9]/g, ''))],
     })
     // console.log(this.state.evolutionSprites[0])
 
@@ -76,13 +60,19 @@ class Pokedex extends React.Component {
       this.setState({
         evolutionIndex: [0, 1],
         evolutionLine: [this.state.evolutionLine[0], evoChain.data.chain.evolves_to[0].species.name],
-        evolutionSprites: [this.state.evolutionSprites[0], this.pokemonSprite(evoChain.data.chain.evolves_to[0].species.name)],
+        evolutionSprites: [this.state.evolutionSprites[0], this.pokemonSprite(evoChain.data.chain.evolves_to[0].species.url.slice(-5).replace(/[^0-9]/g, ''))],
       })
       if (evoChain.data.chain.evolves_to[0].evolves_to.length === 1) {
         this.setState({
           evolutionIndex: [0, 1, 2],
           evolutionLine: [this.state.evolutionLine[0], this.state.evolutionLine[1], evoChain.data.chain.evolves_to[0].evolves_to[0].species.name],
-          evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.pokemonSprite(evoChain.data.chain.evolves_to[0].species.name)],
+          evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.pokemonSprite(evoChain.data.chain.evolves_to[0].evolves_to[0].species.url.slice(-5).replace(/[^0-9]/g, ''))],
+        })
+      } else if (evoChain.data.chain.evolves_to[0].evolves_to.length > 1) {
+        this.setState({
+          evolutionIndex: [0, 1, 2, 3],
+          evolutionLine: [this.state.evolutionLine[0], this.state.evolutionLine[1], evoChain.data.chain.evolves_to[0].evolves_to[0].species.name, evoChain.data.chain.evolves_to[0].evolves_to[1].species.name],
+          evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.pokemonSprite(evoChain.data.chain.evolves_to[0].evolves_to[0].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[0].evolves_to[1].species.url.slice(-5).replace(/[^0-9]/g, ''))],
         })
       }
     } else if (evoChain.data.chain.evolves_to.length > 1) {
@@ -90,19 +80,19 @@ class Pokedex extends React.Component {
       this.setState({
         evolutionIndex: [0, 1, 2],
         evolutionLine: [this.state.evolutionLine[0], evoChain.data.chain.evolves_to[0].species.name, evoChain.data.chain.evolves_to[1].species.name],
-        evolutionSprites: [this.state.evolutionSprites[0], this.pokemonSprite(evoChain.data.chain.evolves_to[0].species.name), this.pokemonSprite(evoChain.data.chain.evolves_to[1].species.name)],
+        evolutionSprites: [this.state.evolutionSprites[0], this.pokemonSprite(evoChain.data.chain.evolves_to[0].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[1].species.url.slice(-5).replace(/[^0-9]/g, ''))],
       })
-      if (evoChain.data.chain.evolves_to.length > 1) {
+      if (evoChain.data.chain.evolves_to.length > 2) {
         this.setState({
           evolutionIndex: [0, 1, 2, 3],
           evolutionLine: [this.state.evolutionLine[0], this.state.evolutionLine[1], this.state.evolutionLine[2], evoChain.data.chain.evolves_to[2].species.name],
-          evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.state.evolutionSprites[2], this.pokemonSprite(evoChain.data.chain.evolves_to[2].species.name)],
+          evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.state.evolutionSprites[2], this.pokemonSprite(evoChain.data.chain.evolves_to[2].species.url.slice(-5).replace(/[^0-9]/g, ''))],
         })
         if (evoChain.data.chain.evolves_to.length === 8) {
           this.setState({
             evolutionIndex: [0, 1, 2, 3, 4, 5, 6, 7],
             evolutionLine: [this.state.evolutionLine[0], this.state.evolutionLine[1], this.state.evolutionLine[2], this.state.evolutionLine[3], evoChain.data.chain.evolves_to[3].species.name, evoChain.data.chain.evolves_to[4].species.name, evoChain.data.chain.evolves_to[5].species.name, evoChain.data.chain.evolves_to[6].species.name, evoChain.data.chain.evolves_to[7].species.name],
-            evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.state.evolutionSprites[2], this.state.evolutionSprites[3], this.pokemonSprite(evoChain.data.chain.evolves_to[3].species.name), this.pokemonSprite(evoChain.data.chain.evolves_to[4].species.name), this.pokemonSprite(evoChain.data.chain.evolves_to[5].species.name), this.pokemonSprite(evoChain.data.chain.evolves_to[6].species.name), this.pokemonSprite(evoChain.data.chain.evolves_to[7].species.name)],
+            evolutionSprites: [this.state.evolutionSprites[0], this.state.evolutionSprites[1], this.state.evolutionSprites[2], this.state.evolutionSprites[3], this.pokemonSprite(evoChain.data.chain.evolves_to[3].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[4].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[5].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[6].species.url.slice(-5).replace(/[^0-9]/g, '')), this.pokemonSprite(evoChain.data.chain.evolves_to[7].species.url.slice(-5).replace(/[^0-9]/g, ''))],
           })
         }
       }
@@ -118,9 +108,8 @@ class Pokedex extends React.Component {
     }
   }
 
-  async pokemonSprite(id){
-    const response = await getPokemonByNumber(id)
-    return response.data.sprites.front_default
+  pokemonSprite(id){
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
   }
 
   handleChange = (event) => {
@@ -195,46 +184,110 @@ class Pokedex extends React.Component {
   }
 
   handleClick4 = (event) => {
+    console.log(event.target.value)
     if (event.target.value !== this.state.name) {
       this.newPokemon(event.target.value)
     }
   }
 
+  handleClick5 = () => {
+    this.setState({
+      openCheck: 'closed animate',
+    })
+  }
+
+  handleClick6 = () => {
+    this.setState({
+      openCheck: 'open',
+    })
+  }
+
   render() {
     if (!this.state) return null
-    const { number, name, sprite, description, flavorText, types, evolutionIndex, evolutionLine, placeholderSprites, value, value2 } = this.state
+    const { number, name, sprite, description, flavorText, types, evolutionIndex, evolutionLine, evolutionSprites, value, value2, openCheck } = this.state
     return (
       <>
         <section>
-          <div className='side1'>
-            <img className='pokemonSprite' src={sprite} />
-            <div className='evoBox'>
-              {evolutionIndex.map(evolution => {
-                return <img onClick={this.handleClick4} value={evolutionLine[evolution]} key={evolution} src={placeholderSprites[evolution]} />
-              })}
-
+          <div className={`side1 ${openCheck}`}>
+            <div className={`onLight ${openCheck}`}></div>
+            <div className={`onButton ${openCheck}`} onClick={this.handleClick6}></div>
+            <div className={`searchInputs ${openCheck}`}>
+              <form onSubmit={this.handleSubmit}>
+                <input className='textInput' type='text' placeholder='Pokemon name' value={value} onChange={this.handleChange} />
+                <input className='numberInput' type='number' placeholder='0' value={value2} onChange={this.handleChange2} />
+                <input className='submitButton' type='submit' value='Search' />
+              </form>
             </div>
-            <div>
-              <button onClick={this.handleClick}>Previous</button>
-              <button onClick={this.handleClick2}>Next</button>
-              <button onClick={this.handleClick3}>Flip</button>
+            <div className={`spriteBox ${openCheck}`}>
+              <img className='pokemonSprite' src={sprite} />
+              <button className='flip' onClick={this.handleClick3}>Flip</button>
+            </div>
+            <div className={`bottomBox ${openCheck}`}>
+              <div className='evoBox'>
+                {evolutionIndex.map(evolution => {
+                  return <img className='evolutionSprite' onClick={this.handleClick4} value={evolutionLine[evolution]} key={evolution} src={evolutionSprites[evolution]} />
+                })}
+              </div>
+              <div className='scrollButtons'>
+                <button className='prev' onClick={this.handleClick}></button>
+                <button className='next' onClick={this.handleClick2}></button>
+              </div>
             </div>
           </div>
-          <div className='side2'>
-            <p>{number}</p>
-            <p>{name}</p>
-            <p>The {description}.</p>
-            <p>{flavorText}</p>
-            <ul>
-              {types.map(indType => {
-                return <li key={indType}>{(indType)}</li>
-              })}
-            </ul>
-            <form onSubmit={this.handleSubmit}>
-              <input type='text' value={value} onChange={this.handleChange} />
-              <input type='number' value={value2} onChange={this.handleChange2} />
-              <input type='submit' value='submit' />
-            </form>
+          <div className='hinge'></div>
+          <div className={`side2 ${openCheck}`}>
+            <div className='closeButton' onClick={this.handleClick5}></div>
+            <div className='infoTop'>
+              <div className='info1'>
+                <h1 className='infoText1'>{name}</h1>
+                <h1 className='infoText1'>#{number}</h1>
+              </div>
+              <h1>The {description}.</h1>
+            </div>
+            <div className='infoBottom'>
+              <div>
+                <ul>
+                  {types.map(indType => {
+                    return <li key={indType}><h1>{(indType)}</h1></li>
+                  })}
+                </ul>
+              </div>
+              <h3 className='flavorText'>{flavorText}</h3>
+            </div>
+            <div className='decorative'>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className='blueSquare'></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+              <div className={`blueSquare change${Math.floor(Math.random() * 4) + 1}`}></div>
+              <div className='blueSquare'></div>
+            </div>
           </div>
         </section>
         {/* <Select className= 'select' isMulti options={typeArray} /> */}
